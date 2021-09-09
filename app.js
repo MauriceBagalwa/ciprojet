be3e76cd1a6132ab1e76b93e330ffd3bec3da212
MauriceBagalwa <mauricebagalwa009@gmail.com>
2021-09-09 16:11:23 +0100

    modif

--- app.js
+++ app.js
@@ -1,25 +1,24 @@
 const express = require("express");
 const app = express();
-require("dotenv").config()
-const PORT = 3760| process.env.PORT;
+require("dotenv").config();
+const PORT = 3760 | process.env.PORT;
 
 app.use(express.json());
 app.get("/", (req, res) => {
-    res.send({
-        status:200,
-        date:{
-            mssg:"hey bro 1"
-        }
-    })
+  res.send({
+    status: 200,
+    date: {
+      mssg: "hey bro 1",
+    },
+  });
 });
 
 app.get("/dash", (req, res) => {
-    res.send({
-        status:200,
-        date:"welcome on my wolrd de Bug Bro"
-    })
+  res.send({
+    status: 200,
+    date: "welcome on my wolrd de Bug Bro...",
+  });
 });
-
 
 app.listen(PORT, () => {
   console.log(`server is runing at :) http://127.0.0.1:${PORT}`);
